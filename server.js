e5b7003bf10e562c9806f3fd587baf94aadff288
gangagamit <gangagamit350@gmail.com>
2024-08-09 09:41:58 +0530

    8-8-24

--- server.js
+++ server.js
@@ -6,35 +6,51 @@ const server = express();
 const fs = require('fs');
 const data = fs.readFileSync('./friend.json','utf-8');
 console.log(data);
+ const morgan =  require('morgan');
 
-    server.get('/',(req,res)=>{
-        res.write('welcome to express js');
-        res.end();
-    })
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
+server.use('/hello',express.static('public'));
+server.use(morgan('tiny'));
 
-    //   server.get('/friend',(req,res)=>{
-    //     res.status(200);
-    //     res.json(JSON.parse(data));
-    // })
-
-let midlwr = (req,res,next)=>{
-    // console.log(req.query);
-
-    if(req.query.password === '456'){
-        console.log('valid')
+let mdlwr = (req,res,next)=>{
+    console.log(req.body);
+    if(req.body.age >=18){
+        console.log('success');
         next();
     }
     else
     {
-        return res.json({message:'invalid password'})
+        return res.json({message:'incorrect way'});
     }
 }
+server.get('/',mdlwr,(req,res)=>{
+    res.write('welcome to express js');
+    res.end();
+})
+//   server.get('/friend',(req,res)=>{
+    //     res.status(200);
+    //     res.json(JSON.parse(data));
+    // })
 
-server.use(midlwr);
+// let midlwr = (req,res,next)=>{
+    // console.log(req.query);
+
+//     if(req.query.password === '456'){
+//         console.log('valid')
+//         next();
+//     }
+//     else
+//     {
+//         return res.json({message:'invalid password'})
+//     }
+// }
+
+// server.use(midlwr);
     
     //CRUD -->  create(post),read(Get),update(Put,Patch),Delete(Delete)
     
-    server.get('/user',midlwr,(req,res)=>{
+    server.get('/user',(req,res)=>{
         res.status(200);
         res.json({message:'user login successfuly'});
     })
@@ -43,6 +59,11 @@ server.use(midlwr);
         res.status(201);
         res.json({message:'user post method'});
     })
+    // server.get('/admin',mdlwr,(req,res)=>{
+    //     res.status(200);
+    //     res.json({message:'user login successfuly'});
+    //     res.end();
+    // })
 
     // server.put('/user',(req,res)=>{
     //     res.status(200);
@@ -58,10 +79,6 @@ server.use(midlwr);
     //     res.status(200);
     //     res.json({message:'user delete method'});
     // })
-server.listen(197,()=>{
-    console.log('server start at http://localhost:197')
-})
-
-
-
-
+server.listen(199,()=>{
+    console.log('server start at http://localhost:199');
+});
