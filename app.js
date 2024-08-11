ac5c2bd5333bd740c5e79848725f7cc9ee040169
gangagamit <gangagamit350@gmail.com>
2024-08-11 18:46:48 +0530

    10-8-2024

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express =  require('express');
 const app = express();
 const products = require('./product.json');
+const users = require('./user.json');
 const morgan = require('morgan');
 
 app.use(express.json());
@@ -10,7 +11,7 @@ app.use(morgan('dev'));
 app.get('/',(req,res)=>{
     res.send('welcome to express js');
   
-})
+});
 
 //create Api
 
@@ -19,15 +20,50 @@ app.post('/product',(req,res)=>{
     res.json({product:req.body,msg:'product add successfully'});
 });
 
+// get all product
 app.get('/product',(req,res)=>{
     res.json(products);
 });
 
-app.get('product/:id',(req,res)=>{
+//get single product
+app.get('/product/:id',(req,res)=>{
     let id =  +req.params.id;
     let item = products.find((product)=>product.id ===  id)
     res.json(item);
 });
+
+//replace data with put method
+
+app.put('/product/:id',(req,res)=>{
+    let id = +req.params.id;
+    let proIndx = products.findIndex((product)=> product.id === id);
+    console.log(proIndx);
+    products.splice(proIndx,1,{...req.body});
+    res.json({message:"product replace successfully"});
+});
+
+//update data with patch method
+
+app.patch('/product/:id',(req,res)=>{
+    let id = +req.params.id;
+    let proIndx = products.findIndex((product)=> product.id === id);
+    console.log(proIndx);
+    const product = products[proIndx];
+    console.log(product);
+    products.splice(proIndx,1,{...product,...req.body});
+    res.json({message:'update succesfully'});
+});
+
+//Delete Data with DELETE method
+app.delete('/product/:id',(req,res)=>{
+    let id = +req.params.id;
+    let proIndx = products.findIndex((product)=>product.id === id);
+    console.log(proIndx);
+    products.splice(proIndx,2);
+    res.json({products,message:"delete successfully"});
+});
+
+
 app.listen(155,()=>{
     console.log('start server');
 })
