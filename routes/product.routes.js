88e2d3d2f7b77647018f8f31047e013bd0f9c65f
gangagamit <gangagamit350@gmail.com>
2024-08-16 08:58:31 +0530

    16-8-24

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,25 +1,42 @@
-const express = require('express');
+// const express = require('express');
 
-const productRoutes = express.Router();
-const {addnewProduct,
-    getallProduct,
-    getProduct,
-    replaceProduct,
-    updateProduct,
-    deleteProduct
+// const productRoutes = express.Router();
+// const {addnewProduct,
+//     getallProduct,
+//     getProduct,
+//     replaceProduct,
+//     updateProduct,
+//     deleteProduct
 
-} = require('../controller/product.controller');
+// } = require('../controller/product.controller');
 
-productRoutes.post('/',addnewProduct);
+// productRoutes.post('/',addnewProduct);
 
-productRoutes.get('/',getallProduct);
+// productRoutes.get('/',getallProduct);
 
-productRoutes.get('/:id',getProduct);
+// productRoutes.get('/:id',getProduct);
 
-productRoutes.put('/:id',replaceProduct);
+// productRoutes.put('/:id',replaceProduct);
 
-productRoutes.patch('/:id',updateProduct);
+// productRoutes.patch('/:id',updateProduct);
 
-productRoutes.delete('/:id',deleteProduct);
+// productRoutes.delete('/:id',deleteProduct);
 
-module.exports = productRoutes;
+// module.exports = productRoutes;
+
+const express = require('express') 
+const productRoutes = express.Router(); 
+ 
+const { 
+    addNewProduct, 
+    getAllProduct, 
+    getProduct 
+ 
+}=require("../controller/product.controller"); 
+ 
+ 
+productRoutes.post("/",addNewProduct); 
+productRoutes.get("/",getAllProduct); 
+productRoutes.get("/get-product",getProduct); 
+ 
+module.exports=productRoutes;
